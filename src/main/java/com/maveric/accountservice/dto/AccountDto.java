405681954c64560153f5fe63cb292ab74b927438
Gradesh Patidar <gradeshp@maveric-systems.com>
2022-09-14 16:46:54 +0530

    Added Validation and Exceptions with all CRUD Operation

--- src/main/java/com/maveric/accountservice/dto/AccountDto.java
+++ src/main/java/com/maveric/accountservice/dto/AccountDto.java
@@ -1,25 +1,29 @@
 package com.maveric.accountservice.dto;
 
-
-import com.maveric.accountservice.constants.Type;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.maveric.accountservice.constants.AccountType;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
-public class AccountDto
-{
-    private String _id;
-    @NotBlank(message = "Customer Id is mandatory")
-    private String customerId;
-    @NotNull(message = "Type is mandatory - 'SAVINGS' or 'CURRENT'")
-    private Type type;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+public class AccountDto {
+        private String _id;
+        @NotBlank(message = "Customer Id is mandatory")
+        private String customerId;
+        @NotNull(message = "Type is mandatory - 'SAVINGS' or 'CURRENT'")
+        private Type type;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private BalanceDto balance;
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private List<TransactionDto> transactions;
 }
